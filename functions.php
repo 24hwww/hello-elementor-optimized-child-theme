c79934fa14acb9f26dcb6355e6282f84a8d57ef0
Leonardo Reyes <24hwww@gmail.com>
2024-10-18 09:07:52 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -22,7 +22,6 @@ define( 'HELLO_ELEMENTOR_CHILD_VERSION', '1.0.0' );
  * @return void
  */
 
-
 class HelloElementorOptimizedChild{
 	private static $instance = null;
 
@@ -34,12 +33,14 @@ class HelloElementorOptimizedChild{
     }
     function __construct() {
 
-    	add_action( 'wp_enqueue_scripts', array($this,'hello_elementor_child_scripts_styles'), 20 );
-		add_action( 'wp_head', array($this,'wp_head_func'));
-
+    	add_action('wp_enqueue_scripts', array($this,'hello_elementor_child_scripts_styles'), 20 );
+	add_action('wp_head', array($this,'wp_head_func'));
+	add_action('wp_head', array($this,'capturar_contenido_head'), 0);
+	add_action('wp_head', array($this,'agrupar_meta_y_link'), 999);
+	    
     }
 	public function hello_elementor_child_scripts_styles() {
-
+	
 		wp_enqueue_style('hello-elementor-child-style',get_stylesheet_directory_uri() . '/style.css',['hello-elementor-theme-style'], HELLO_ELEMENTOR_CHILD_VERSION);
 	
 	}
@@ -53,6 +54,25 @@ class HelloElementorOptimizedChild{
 		<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
 		<link rel="profile" href="https://gmpg.org/xfn/11">    
 		<?php
+	}
+
+	public function capturar_contenido_head(){
+		ob_start();
+	}
+
+	public function agrupar_meta_y_link(){
+	    $head_content = ob_get_clean();
+	    preg_match_all('/<meta[^>]+>/', $head_content, $meta_tags);
+	    preg_match_all('/<link[^>]+>/', $head_content, $link_tags);
+	    $head_content_sin_meta_link = preg_replace('/<(meta|link)[^>]+>/', '', $head_content);
+	    echo '<!-- ola.marketing -->';
+	    if (!empty($meta_tags[0])) {
+	        echo implode("\n", $meta_tags[0]) . "\n";
+	    }
+	    if (!empty($link_tags[0])) {
+	        echo implode("\n", $link_tags[0]) . "\n";
+	    }
+	    echo $head_content_sin_meta_link;
 	}
 
 }
